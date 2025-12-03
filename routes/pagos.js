cfdf75f6b10c39978773e8c5bbfcbe6691a36491
tiendacabana685-ai <tiendacabana685@gmail.com>
2025-12-03 17:02:26 +0000

    Delete routes directory

--- routes/pagos.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { crearIntentoPago } from "../controllers/pagosController.js";
-
-const router = express.Router();
-
-router.post("/create-payment-intent", crearIntentoPago);
-
-export default router;
